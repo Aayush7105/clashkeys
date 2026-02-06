8fc05e0d0862aee25d08316a3ce0eb312204c36f
Aayush7105 <aayushrawat5107@gmail.com>
2026-02-07 00:30:37 +0530

    score page added

--- components/landing/landingpage.tsx
+++ components/landing/landingpage.tsx
@@ -6,13 +6,21 @@ const Landing = () => {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen w-full flex justify-center items-center flex-col">
-      <h1>ClashKeys</h1>
-      <div className="flex gap-5">
-        <button onClick={() => router.push("/multiplayer")}>
+    <div className="min-h-screen w-full flex justify-center items-center flex-col gap-5">
+      <h1 className="text-4xl">ClashKeys</h1>
+      <div className="flex gap-10">
+        <button
+          onClick={() => router.push("/multiplayer")}
+          className="border px-2 py-1 rounded-xl"
+        >
           Play with Friends
         </button>
-        <button onClick={() => router.push("/soloplay")}>Practice now</button>
+        <button
+          onClick={() => router.push("/soloplay")}
+          className="border px-2 py-1 rounded-xl"
+        >
+          Practice now
+        </button>
       </div>
     </div>
   );
